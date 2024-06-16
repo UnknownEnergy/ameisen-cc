1b819a6fc512143d8a130162111938409e131f66
daniel <daniel06061@gmail.com>
2024-06-16 11:57:46 +0200

    Update

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,14 +1,14 @@
 import { Boot } from './scenes/Boot';
 import { Game as MainGame } from './scenes/Game';
-import { AUTO, Game } from 'phaser';
+import {AUTO, CANVAS, Game} from 'phaser';
 import { Preloader } from './scenes/Preloader';
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Phaser.Types.Core.GameConfig = {
     type: AUTO,
-    width: window.innerWidth-100,
-    height: window.innerHeight-100,
+    width: 1024,
+    height: 768,
     parent: 'game-container',
     backgroundColor: '#000000',
     dom: {
