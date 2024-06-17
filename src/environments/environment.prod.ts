16f753de347af64934326ac30318a59a33679042
daniel <daniel06061@gmail.com>
2024-06-17 23:27:25 +0200

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://grazer.duckdns.org:3000',
+    apiUrl: 'https://[2a02:1748:ff10:0:5005:16f5:54a9:cd0d]:3000/',
     googleClientId: '340545855934-fh3pjeqo9c2mmtbg10aa5e4bc3n1e447.apps.googleusercontent.com'
 };
